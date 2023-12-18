90edaf2475c69661cf1c10172c2a65796ff98cd2
anema25 <116646218+anema25@users.noreply.github.com>
2023-12-18 19:31:46 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,41 +1,61 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+import streamlit as st
 from joblib import load
+import requests
+from bs4 import BeautifulSoup
 
-presence_classifier = load('presence_classifier.joblib')
-presence_vect = load('presence_vectorizer.joblib')
-category_classifier = load('category_classifier.joblib')
-category_vect = load('category_vectorizer.joblib')
+# Load your models
+presence_classifier = load('api/presence_classifier.joblib')
+presence_vect = load('api/presence_vectorizer.joblib')
+category_classifier = load('api/category_classifier.joblib')
+category_vect = load('api/category_vectorizer.joblib')
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/', methods=['POST'])
+# Streamlit app
 def main():
-    if request.method == 'POST':
-        output = []
-        data = request.get_json().get('tokens')
+    st.title("Dark Pattern Detection")
 
-        for token in data:
-            result = presence_classifier.predict(presence_vect.transform([token]))
-            if result == 'Dark':
-                cat = category_classifier.predict(category_vect.transform([token]))
-                output.append(cat[0])
-            else:
-                output.append(result[0])
+    # Get URL input from user
+    url = st.text_input("Enter website URL:", "")
 
-        dark = [data[i] for i in range(len(output)) if output[i] == 'Dark']
-        for d in dark:
-            print(d)
-        print()
-        print(len(dark))
+    if st.button("Fetch and Detect Dark Patterns"):
+        text_from_website = fetch_text_from_website(url)
+        tokens = text_from_website.split('\n')  # Assuming you want to split text into tokens
 
-        message = '{ \'result\': ' + str(output) + ' }'
-        print(message)
+        output = detect_dark_patterns(tokens)
+        st.json({"result": output})
 
-        json = jsonify(message)
+def fetch_text_from_website(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = ' '.join([p.get_text() for p in soup.find_all('p')])  # Extract text from paragraphs
+        return text
+    except Exception as e:
+        st.error(f"Error fetching text from the website: {e}")
+        return ""
 
-        return json
+def detect_dark_patterns(tokens):
+    # The rest of your detection code remains unchanged
+    output = []
+
+    for token in tokens:
+        result = presence_classifier.predict(presence_vect.transform([token]))
+        if result == 'Dark':
+            cat = category_classifier.predict(category_vect.transform([token]))
+            output.append(cat[0])
+        else:
+            output.append(result[0])
+
+    dark_patterns = [tokens[i] for i in range(len(output)) if output[i] == 'Dark']
+    st.text("Detected Dark Patterns:")
+    for d in dark_patterns:
+        st.text(d)
+    
+    st.text(f"Number of Detected Dark Patterns: {len(dark_patterns)}")
+
+    st.text("Debugging:")
+    st.text(f"Output of presence classifier: {output}")
+    
+    return output
 
 if __name__ == '__main__':
-    app.run(threaded=True, debug=True)
+    main()
